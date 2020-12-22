774d430168403499946e9c88f1c16a4c27d3aee4
Michał Malik <michalmmalik@gmail.com>
2020-12-22 12:56:43 +0100

    refactor: do not use admin path

--- packages/cms/gatsby-config.js
+++ packages/cms/gatsby-config.js
@@ -8,7 +8,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.ts`,
+        modulePath: "./src/cms/cms.ts",
+        publicPath: "",
       },
     },
   ],
