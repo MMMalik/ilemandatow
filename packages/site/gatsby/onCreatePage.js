baf34073e0eec6da641ccbb57e3dfa054590816c
Michał Malik <michalmmalik@gmail.com>
2021-01-07 14:18:47 +0100

    Refactor/cms (#27)
    * refactor(cms): move content and locales to separate packages
    
    * refactor(cms): adjust config and deployment
    
    * refactor(cms): adjust config further
    
    * refactor(cms): use prerelease versioning

--- packages/site/gatsby/onCreatePage.js
+++ packages/site/gatsby/onCreatePage.js
@@ -1,4 +1,4 @@
-const localesMeta = require("../locales/meta");
+const { meta, translations } = require("@ilemandatow/locales");
 const { removeTrailingSlash } = require("./utils");
 
 exports.onCreatePage = async ({ page, actions, reporter }) => {
@@ -6,15 +6,11 @@ exports.onCreatePage = async ({ page, actions, reporter }) => {
 
   deletePage(page);
 
-  const localesByKey = Object.keys(localesMeta);
+  const localesByKey = Object.keys(meta);
   const is404 = page.path.includes("404");
 
-  const getTranslations = (langKey) => {
-    return require(`../locales/translations/${langKey}.json`);
-  };
-
   if (is404) {
-    const defaultLang = localesByKey.find((lang) => localesMeta[lang].default);
+    const defaultLang = localesByKey.find((lang) => meta[lang].default);
 
     if (!defaultLang) {
       return reporter.panicOnBuild("Default language was not found.");
@@ -25,22 +21,22 @@ exports.onCreatePage = async ({ page, actions, reporter }) => {
       context: {
         ...page.context,
         i18nCtx: {
-          ...localesMeta[defaultLang],
-          translations: getTranslations(defaultLang),
+          ...meta[defaultLang],
+          translations: translations[defaultLang],
         },
       },
     });
   } else {
-    Object.keys(localesMeta).map((lang) => {
-      const localizedPath = `/${localesMeta[lang].path}${page.path}`;
+    Object.keys(meta).map((lang) => {
+      const localizedPath = `/${meta[lang].path}${page.path}`;
       return createPage({
         ...page,
         path: removeTrailingSlash(localizedPath),
         context: {
           ...page.context,
           i18nCtx: {
-            ...localesMeta[lang],
-            translations: getTranslations(lang),
+            ...meta[lang],
+            translations: translations[lang],
           },
         },
       });
