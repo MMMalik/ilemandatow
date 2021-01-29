8cb3e21746652ec4fc7c054ed8513e41d1ebf98f
Michał Malik <michalmmalik@gmail.com>
2021-01-29 20:07:11 +0100

    refactor(keystone): test deploy workflow

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -14,18 +14,20 @@ yarn netlify deploy --dir=packages/site/public --prod
 # Add ssh key
 # Requires MD_SSH_KEY, MD_SSH_HOST
 mkdir ~/.ssh
-echo $MD_SSH_KEY >~/.ssh/id_ed25519
-ssh-keyscan -H $MD_SSH_HOST >>~/.ssh/known_hosts
+printf $MD_SSH_KEY >~/.ssh/id_ed25519
+ssh-keyscan -H $MD_SSH_HOST >~/.ssh/known_hosts
+
+# Create .env file
+# Requires variables: DB_USER, DB_PASS, DB_NAME, DB_URI, COOKIE_SECRET
+printf "DB_USER=$DB_USER\nDB_PASS=$DB_PASS\nDB_NAME=$DB_NAME\nDB_URI=$DB_URI\nCOOKIE_SECRET=$COOKIE_SECRET" >packages/api/.env
 
 # Copy API files
 # Requires variables: MD_SSH_USER, MD_SSH_HOST, MD_SSH_DOMAIN
-rsync -zavh \
+rsync -zavhR \
     app.js \
     node_modules \
-    packages/api/build \
-    packages/api/dist \
-    packages/api/index.js \
-    MD_SSH_USER@MD_SSH_HOST:/usr/home/MD_SSH_USER/domains/MD_SSH_DOMAIN/public_nodejs
+    packages/api \
+    $MD_SSH_USER@$MD_SSH_HOST:/usr/home/$MD_SSH_USER/domains/$MD_SSH_DOMAIN/public_nodejs
 
 # Restart server
-ssh MD_SSH_USER@MD_SSH_HOST "devil www restart $MD_SSH_DOMAIN"
+ssh $MD_SSH_USER@$MD_SSH_HOST "devil www restart $MD_SSH_DOMAIN"
