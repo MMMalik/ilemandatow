e68ede13b5af4bd499f0a662eb7bf47bcab5ef79
Michał Malik <michalmmalik@gmail.com>
2021-02-03 14:21:25 +0100

    fix(deploy): copy db:restore script

--- .github/workflows/deploy-api.sh
+++ .github/workflows/deploy-api.sh
@@ -31,7 +31,7 @@ ssh-keyscan -H $MD_SSH_HOST >~/.ssh/known_hosts
 # Creates db script wrappers
 printf "\n%s\n\n" "Create scripts directory"
 mkdir ~/.db-scripts
-for i in db:backup db:backup:cleanup db:setup db:seed; do
+for i in db:backup db:backup:cleanup db:restore db:setup db:seed; do
     printf "\n%s\n\n" "Create $i script wrapper"
     printf "cd public_nodejs && NODE_ENV=production API_ENV_PATH=$DEST_PATH/.env yarn $i" >~/.db-scripts/$i.sh
 done
