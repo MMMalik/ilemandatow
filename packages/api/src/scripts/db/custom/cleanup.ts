1f4f64be84cba0bd940776c42406dc31216bed5d
Michał Malik <michalmmalik@gmail.com>
2021-02-03 12:08:33 +0100

    feat(db-scripts): create and upload db scripts

--- packages/api/src/scripts/db/custom/cleanup.ts
+++
@@ -1,9 +0,0 @@
-import { exec } from "shelljs";
-import { join } from "path";
-import { setupPgEnv } from "../../../config";
-
-// Setups env variables for source database
-setupPgEnv();
-
-// Truncates all tables
-exec(`psql -f ${join(__dirname, "../raw", "truncateTables.sql")}`);
