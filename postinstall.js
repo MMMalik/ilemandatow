64f88aaa13ba64c198a55723ccbf101ffc547fb5
Michał Malik <michalmmalik@gmail.com>
2021-02-03 13:34:22 +0100

    fix: add separate postinstall script

---
+++ postinstall.js
@@ -0,0 +1,12 @@
+const { exec, test } = require("shelljs");
+
+const huskyBinExists = test("-f", "node_modules/.bin/husky");
+
+if (huskyBinExists) {
+  exec("yarn husky install");
+} else {
+  // This block is evaluated if `yarn install --production` was run.
+  console.warn(
+    "`husky` command could not be run. Most likely it's not installed."
+  );
+}
