51e58c8950c6b3c140bc058ac7ed4bf0bd8a0ac6
Michał Malik <michalmmalik@gmail.com>
2021-02-09 17:47:41 +0100

    feat(client): cleanup removed imports

--- packages/site/src/types/graphql.ts
+++
@@ -1,3 +0,0 @@
-// Generating types directly inside <root>/src triggers infinite loop.
-// Therefore, types are generated in <root>/generated and re-exported from here.
-export * from "../../generated/graphql";
--- packages/site/src/types/index.ts
+++ packages/site/src/types/index.ts
@@ -1,3 +1,2 @@
-export * from "./graphql";
 export * from "./i18nCtx";
 export * from "./pageContext";
