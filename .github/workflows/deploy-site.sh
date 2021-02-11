9ef60e450b629011adc33a32bb35027c18601a0a
Michał Malik <michalmmalik@gmail.com>
2021-02-11 13:29:17 +0100

    feat(client): create .env file for browser build

--- .github/workflows/deploy-site.sh
+++ .github/workflows/deploy-site.sh
@@ -4,6 +4,11 @@ set -e
 printf "\n%s\n\n" "Install dependencies"
 yarn install
 
+# Creates .env.production file
+# Requires variables: ILEMANDATOW_ALLOW_ROBOTS, ILEMANDATOW_API_URL, ILEMANDATOW_SITE_URL
+printf "\n%s\n\n" "Save environment variables to .env.production"
+printf "ILEMANDATOW_ALLOW_ROBOTS=$ILEMANDATOW_ALLOW_ROBOTS\nILEMANDATOW_API_URL=$ILEMANDATOW_API_URL\nILEMANDATOW_SITE_URL=$ILEMANDATOW_SITE_URL" >./packages/site/.env.production
+
 # Builds project
 printf "\n%s\n\n" "Build project and its dependencies"
 yarn build
