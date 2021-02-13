37af2344dc3ec461a6acd29a20023e32577aa8be
Michał Malik <michalmmalik@gmail.com>
2021-02-13 13:47:52 +0100

    feat(404): add styling for 404 page

--- packages/site/src/pages/404.tsx
+++ packages/site/src/pages/404.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
-import { useTranslation } from "../i18n";
+import { Link } from "@ilemandatow/ui";
+import { useRoutes } from "../routes";
 
+/**
+ * Please note that 404 page is outside of language context.
+ * Polish phrases are therefore hardcoded.
+ */
 const Page404: React.FC = () => {
-  const { t } = useTranslation();
+  const { routes } = useRoutes();
+
   return (
-    <>
-      <div>{t("pageNotFound")}</div>
-      <div>{t("oopsNotExists")}</div>
-    </>
+    <div className="tc">
+      <div className="f2">{"404"}</div>
+      <div className="f4 mt3">{"Ta strona nie istnieje"}</div>
+      <div className="mt3">
+        <Link to={routes.home.link()}>{"Wróć do strony głównej"}</Link>
+      </div>
+    </div>
   );
 };
 
